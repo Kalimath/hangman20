5ee6e101a145f71ba95d87b4ac1e56d7a254ff54
Vincent <vincentvandenbosch19@gmail.com>
2018-04-17 14:41:22 +0200

    Story 4 begin

---
+++ src/Domain/Rechthoek.java
@@ -0,0 +1,26 @@
+package Domain;
+
+public class Rechthoek {
+	private int breedte; 
+	private int hoogte;
+	
+	
+	public int getBreedte() {
+		return breedte;
+	}
+	
+	
+	public void setBreedte(int breedte) {
+		this.breedte = breedte;
+	}
+	
+	
+	public int getHoogte() {
+		return hoogte;
+	}
+	
+	
+	public void setHoogte(int hoogte) {
+		this.hoogte = hoogte;
+	}	
+}
