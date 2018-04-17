c54e8727b9eed456858559d83ab20163b0f4b209
kalimath <mathieu_broe@yahoo.com>
2018-04-17 15:07:26 +0200

    Auto stash before merge of "master" and "origin/master"
    Story 5 met 2 onvopgeloste errors ;-)

--- src/ui/Launcher.java
+++ src/ui/Launcher.java
@@ -5,24 +5,29 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 import Domain.DB;
+import Domain.Lijnstuk;
+import Domain.Punt;
 import Domain.Speler;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Speler> db = new ArrayList<>();
-	
-		String naam = JOptionPane.showInputDialog("Welkom!!\nHoeheet je?");
-		Speler p = new Speler(naam);
+//		ArrayList<Speler> db = new ArrayList<>();
+//	
+//		String naam = JOptionPane.showInputDialog("Welkom!!\nHoeheet je?");
+//		Speler p = new Speler(naam);
+//		
+//		db.add(p);
+//		
+//		for(int i = 0; i < db.size(); i++) {
+//			
+//			JOptionPane.showMessageDialog(null, db.get(i).getNaam()+" heeft score: "+db.get(i).getScore(), db.get(i).getNaam(), JOptionPane.INFORMATION_MESSAGE);
+//			
+//		}
 		
-		db.add(p);
-		
-		for(int i = 0; i < db.size(); i++) {
-			
-			JOptionPane.showMessageDialog(null, db.get(i).getNaam()+" heeft score: "+db.get(i).getScore(), db.get(i).getNaam(), JOptionPane.INFORMATION_MESSAGE);
-			
-		}
+		Lijnstuk a = new Lijnstuk(new Punt(1, 1), new Punt(2, 3));
+		System.out.println(a.toString());
 		
 		
 		
